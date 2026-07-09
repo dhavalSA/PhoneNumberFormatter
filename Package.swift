bc28e11fcd2f365ad9b3191abe64a34f9bf2f872
Dhaval soni <dhaval.soni@solutionanalysts.com>
2026-07-09 11:06:03 +0530

    [Feature]Added the SPM.

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "PhoneNumberFormatter",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+        .tvOS(.v13),
+        .watchOS(.v7)
+    ],
+    products: [
+        .library(
+            name: "PhoneNumberFormatter",
+            targets: ["PhoneNumberFormatter"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "PhoneNumberFormatter",
+            path: "PhoneNumberFormatter/Sources"
+        ),
+        .testTarget(
+            name: "PhoneNumberFormatterTests",
+            dependencies: ["PhoneNumberFormatter"],
+            path: "PhoneNumberFormatterTests"
+        )
+    ]
+)
